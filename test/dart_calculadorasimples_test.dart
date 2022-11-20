1cc5fae5724371f8cb9ae8591d9cef4cf08968fd
Elbis Silva <elbissilva@gmail.comm>
2022-11-20 20:24:36 -0300

    commit inicial

---
+++ test/dart_calculadorasimples_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_calculadorasimples/dart_calculadorasimples.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
