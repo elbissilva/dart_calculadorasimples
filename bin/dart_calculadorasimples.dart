1211cdb7a40b28d11562efc136fb4f63a4179356
Elbis Silva <elbissilva@gmail.comm>
2022-11-22 22:44:57 -0300

    commit

--- bin/dart_calculadorasimples.dart
+++ bin/dart_calculadorasimples.dart
@@ -6,24 +6,18 @@ import 'package:dart_calculadorasimples/dart_calculadorasimples.dart' as dart_ca
 void main(List<String> arguments) {
   print("Calculadora simples");
   
-  print('Informe o primeiro Número:');
-  var line = stdin.readLineSync(encoding:utf8);
-  var numero1 = num.parse(line ?? "0");
+ 
+  var numero1 = lerConsoleDouble('Informe o primeiro Número:');
 
-
-   print('Informe o segundo Número:');
-  line = stdin.readLineSync(encoding:utf8);
-  var numero2 = num.parse(line?? "0");
-
-  print('Informe a operaçã matemática(+, -, *, /):');
-  line = stdin.readLineSync(encoding:utf8);
-  var operacao = line?? "";
+  var numero2 = lerConsoleDouble('Informe o segundo Número:');
+  
+  var operacao =lerConsole('Informe a operaçã matemática(+, -, *, /):');
+  calcular(operacao, numero1, numero2);
 
   print(numero1);
   print(numero2);
   print(operacao);
 
-  num resultado  = 0;
 //=========================USANDO IF ELSE=============================================
   // if (operacao =='+'){
   //   resultado = numero1 + numero2;
@@ -39,18 +33,76 @@ void main(List<String> arguments) {
   // }
   
   //=============================SWHITCH CASE============================================
+  // switch (operacao) {
+  //   case "+":
+  //   resultado = soma(numero1, numero2);
+  //   break;
+  //   case "-":
+  //   resultado = sub(numero1, numero2);
+  //   break;
+  //   case "*":
+  //   resultado = mult(numero1, numero2);
+  //   break;
+  //   case "/":
+  //   resultado = div(numero1, numero2);
+  //   break;
+  //   default:
+  //    print('Operação inválida');
+  //    exit(0);
+  // }
+  // print("O resultado da operção é:$resultado");
+
+  //=================Usando FUNÇÕES===============================================
+}
+double soma(double numero1, double numero2){
+  return  numero1 + numero2;
+}
+
+double sub(double numero1, double numero2){
+  return numero1 - numero2;
+}
+
+double mult(double numero1, double numero2){
+  return numero1* numero2;
+}
+
+double div(double numero1, double numero2){
+  if (numero2==0){
+    print("Não é possivel dividir por zero!");
+    exit(0);
+  } else {
+  return numero1/numero2;
+  }
+}
+
+String lerConsole(String texto){
+  var line = stdin.readLineSync(encoding:utf8);
+  return line ?? "";
+}
+
+double lerConsoleDouble(String texto){
+  var numero =double.tryParse(lerConsole(texto));
+  if (numero == null){
+    return 0.0;
+  } else{
+    return numero;
+  }
+}
+
+void calcular (String operacao, double numero1, double numero2){
+  double resultado  = 0;
   switch (operacao) {
     case "+":
-    resultado = numero1+numero2;
+    resultado = soma(numero1, numero2);
     break;
     case "-":
-    resultado = numero1-numero2;
+    resultado = sub(numero1, numero2);
     break;
     case "*":
-    resultado = numero1*numero2;
+    resultado = mult(numero1, numero2);
     break;
     case "/":
-    resultado = numero1/numero2;
+    resultado = div(numero1, numero2);
     break;
     default:
      print('Operação inválida');
@@ -58,4 +110,3 @@ void main(List<String> arguments) {
   }
   print("O resultado da operção é:$resultado");
 }
-
